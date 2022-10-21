9d0260c82967cd955cc90abe08c343e14118dcaf
Semih AÇMALI <suhedasemihacmali@gmail.com>
2022-10-21 14:02:42 +0300

    Add files via upload

--- src/Hafta3/Odev.java
+++ src/Hafta3/Odev.java
@@ -1,5 +1,15 @@
 package Hafta3;
 
 public class Odev {
-
+	/*
+	Fonksiyona girdi olarak verilen metin icerisinde kucuk harf olarak yazilan harfi buyuk harfe, buyuk harf olarak verilen harfi kucuk harfe ceviren
+	bir fonksiyon yaziniz.
+	Örn;
+	Fonksiyonunun adi; harfDonusturme
+	harfDonusturme("Yuksek! İhtisas? Universitesi")
+	Sonuc; "yUKSEK! iHTİSAS? uNİVERSİTESİ"
+	
+	Not; Bosluklara ve diger ifadelere duyarli olmali ve onlarda degisiklik yapmamali
+	Onceki Odevlerden biri olan ASCII TABLE odevi size bu konuda yardimci olacaktir.
+	*/
 }
